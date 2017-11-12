eab6098b8a30c2c7dabe0e5900cb1da417817db6
lysastrata <celebrian94@gmail.com>
2017-11-11 22:49:05 -0800

    added templates into controller

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,30 +1,30 @@
-var score = require('../controllers/scores.js');
+var users = require('../controllers/users.js');
 var path = require('path');
 
 module.exports = function (app){
     console.log("in routes");
 
-    app.get('/api/scores', function (req, res){
-        console.log("in get req")
-        score.showScores(req, res);
+    app.get('/api/templates', function (req, res){
+        console.log("in get req templates")
+        users.showTemplates(req, res);
     });
 
-    app.get('/api/questions', function (req, res){
-        console.log("in get req")
-        score.showQuestions(req, res);
-    });
+    // app.get('/api/questions', function (req, res){
+    //     console.log("in get req")
+    //     score.showQuestions(req, res);
+    // });
 
-    app.post('/api/scores/create', function (req, res){
-        console.log("in routes.js post add")
-        score.addScore(req, res);
-    });
+    // app.post('/api/scores/create', function (req, res){
+    //     console.log("in routes.js post add")
+    //     score.addScore(req, res);
+    // });
 
-    app.post('/api/questions/create', function (req, res){
-        console.log("in routes.js post create")
-        score.create(req, res);
-    });
+    // app.post('/api/questions/create', function (req, res){
+    //     console.log("in routes.js post create")
+    //     score.create(req, res);
+    // });
 
-    app.all("*", (req,res,next) => {
-        res.sendFile(path.resolve("./TriviaApp/dist/index.html"))
-    });
+    // app.all("*", (req,res,next) => {
+    //     res.sendFile(path.resolve("./TriviaApp/dist/index.html"))
+    // });
 }
