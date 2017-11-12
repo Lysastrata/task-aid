0dc8e1933a1886d458133c38cc15d1dad55df097
lysastrata <celebrian94@gmail.com>
2017-11-11 21:16:18 -0800

    check

---
+++ server/models/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new mongoose.Schema({
+    buisnessName: { type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    cellphone:{type: Number, required: true},
+    website: {type: String, required: true},
+    zipcode: {type: Number, required: true},
+    _templates: [{type: Schema.Types.ObjectId, ref: 'Template'}]
+});
+var User = mongoose.model('User', UserSchema);
+
+var TemplateSchema = new mongoose.Schema({
+    name: { type: String, required: true},
+    nodes: [{type: Object}],
+    _user: {type: Schema.Types.ObjectId, ref: 'User'}
+});
+var Template = mongoose.model('Template', TemplateSchema);
+
